43299bb29b9dd24e05f420a0cc48d767eb0c09c7
Dmitry <vasilishin.d.o@gmail.com>
2017-02-20 14:36:38 +0200

    gunicorn config

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -8,10 +8,10 @@ def numCPUs():
         raise RuntimeError("No sysconf detected.")
     return os.sysconf("SC_NPROCESSORS_ONLN")
 
-port = os.environ.get('GUNICORN_PORT', 9003)
+port = os.environ.get('GUNICORN_PORT', 9006)
 
 bind = "127.0.0.1:%s" % port
-workers = 4
+workers = 3
 worker_class = "gevent"
 max_requests = 1000
 daemon = True
@@ -19,5 +19,5 @@ pidfile = TMP + "dash_stats_g.pid"
 accesslog = TMP + "dash_stats_g.access.log"
 errorlog = TMP + "dash_stats_g.error.log"
 graceful_timeout = 60
-timeout = 500
+timeout = 300
 keepalive = 0
