6e0921def481b204932e8a2b8a8fb5a35721561b
Yuliya Krashanovskaya <yuliya.krashanov@redwerk.com>
2017-02-28 19:38:19 +0200

    added managenent commands for creating superuser and aupdating projects, change api views methods

--- manage.py
+++ manage.py
@@ -1,13 +1,15 @@
-from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 from dashboard import app
 from dashboard.database import settings_db
-
+from dashboard.commands import UpdateProjectsCommand, CreateSuperUserCommand
 
 migrate = Migrate(app, settings_db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+manager.add_command('updateprojects', UpdateProjectsCommand)
+manager.add_command('createsuperuser', CreateSuperUserCommand)
 
 
 if __name__ == '__main__':
